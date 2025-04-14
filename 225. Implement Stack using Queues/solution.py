d31b3e2707f9b3ce975b8e63deea9526218b7bd1
rasthpop <rasthetrollman@example.com>
2025-04-14 16:12:24 +0300

    implementation of a stack using queues

--- 225. Implement Stack using Queues/solution.py
+++ 225. Implement Stack using Queues/solution.py
@@ -1,21 +1,22 @@
-
 class MyStack:
 
     def __init__(self):
         self.left_q = []        
-        self.right_q = []        
+        # self.right_q = []        
 
     def push(self, x: int) -> None:
-        
+        self.left_q.append(x)
 
     def pop(self) -> int:
+        return self.left_q.pop()
         
 
     def top(self) -> int:
-        
+        return self.left_q[-1]
 
     def empty(self) -> bool:
-        
+        return not self.left_q
+
 
 
 # Your MyStack object will be instantiated and called as such:
