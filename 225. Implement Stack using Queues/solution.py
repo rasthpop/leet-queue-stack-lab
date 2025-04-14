6c52f3ff984c61d9c131ff29c69f29b19cd8ae6f
rasthpop <rasthetrollman@example.com>
2025-04-14 16:09:02 +0300

    implemented queue using stacks 232

---
+++ 225. Implement Stack using Queues/solution.py
@@ -0,0 +1,26 @@
+
+class MyStack:
+
+    def __init__(self):
+        self.left_q = []        
+        self.right_q = []        
+
+    def push(self, x: int) -> None:
+        
+
+    def pop(self) -> int:
+        
+
+    def top(self) -> int:
+        
+
+    def empty(self) -> bool:
+        
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
---
+++ 232. Implement Queue using Stacks/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def __init__(self):
+        self.first_stack = []
+        self.second_stack = []
+
+    def push(self, value: int) -> None:
+        self.first_stack.append(value)
+
+    def pop(self) -> int:
+        self.peek() 
+        return self.second_stack.pop()
+
+    def peek(self) -> int:
+        if not self.second_stack:
+            while self.first_stack:
+                self.second_stack.append(self.first_stack.pop())
+        return self.second_stack[-1]
+
+    def empty(self) -> bool:
+        return not self.first_stack and not self.second_stack
